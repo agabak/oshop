454ab2406738d5c1f822e58c1900cbcfcf1c61b1
Agaba Kabunga <agaba_k@hotmail.com>
2019-07-22 22:09:42 -0500

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDWdaVsGmGItTat_aflYEncH8-WqGE0Wv4',
+    authDomain: 'oshop-b782d.firebaseapp.com',
+    databaseURL: 'https://oshop-b782d.firebaseio.com',
+    projectId: 'oshop-b782d',
+    storageBucket: '',
+    messagingSenderId: '549305451902',
+    appId: '1:549305451902:web:1136c89daf29acb4'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDWdaVsGmGItTat_aflYEncH8-WqGE0Wv4',
+    authDomain: 'oshop-b782d.firebaseapp.com',
+    databaseURL: 'https://oshop-b782d.firebaseio.com',
+    projectId: 'oshop-b782d',
+    storageBucket: '',
+    messagingSenderId: '549305451902',
+    appId: '1:549305451902:web:1136c89daf29acb4'
+  }
 };
 
 /*
